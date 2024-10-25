93c35223ecb02f0c55a837f5f503e2b38f2e95ac
Sylver-Wolf <djfalcon37510@gmail.com>
2024-10-25 14:33:28 +0200

    debut de test

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button, Pressable } from 'react-native';
 
-export default function App() {
+export default function App(props) {
+  const { onPress, title = 'Save' } = props;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Coucou !</Text>
+      <Button
+        title='OUI²'
+        color="#fff"
+        onPress={() => Alert.alert('Je suis le bouton 1 :D')} />
       <StatusBar style="auto" />
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>{title}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -16,5 +24,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
